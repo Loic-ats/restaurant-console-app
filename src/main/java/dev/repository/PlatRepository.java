2aaa2420cfc180396ee8e5b7643428e4d063a540
Loïc <you@example.com>
2020-09-03 17:25:47 +0200

    TP 4.4 V03

--- src/main/java/dev/repository/PlatRepository.java
+++ src/main/java/dev/repository/PlatRepository.java
@@ -1,9 +1,27 @@
 package dev.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import dev.entite.Ingredient;
 import dev.entite.Plat;
 
 public interface PlatRepository extends JpaRepository<Plat, Integer> {
+
+	List<Plat> findByPrixEnCentimesEuros(Integer prix);
+
+	@Query("select avg(p.prixEnCentimesEuros) from Plat p where p.prixEnCentimesEuros > ?1 ")
+	Double findByAvgPrixSuppA(Integer prixEnCentimeEuros);
+
+	@Query("select ing from Ingredient ing left join ing.listPlatsQuiContiennentIng p where p.nom = ?1")
+	List<Ingredient> findIngredientByPlatNom(String nom);
+
+	@Modifying
+	@Query("UPDATE plat p SET nom = :nouveauNom WHERE p.nom = :ancienNom")
+	void updateNomPlat(@Param("ancienNom") String ancienNom, @Param("nouveauNom") String nouveauNom);
 
 }
--- src/test/java/dev/repository/PlatRepositoryIntegrationTest.java
+++ src/test/java/dev/repository/PlatRepositoryIntegrationTest.java
@@ -2,8 +2,19 @@ package dev.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.awt.print.Pageable;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.assertj.core.data.Index;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,10 +22,11 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import dev.config.JdbcTestConfig;
 import dev.config.JpaConfig;
+import dev.entite.Ingredient;
+import dev.entite.Plat;
 
 @SpringJUnitConfig({ JdbcTestConfig.class, // DataSourceH2, JdbcTemplate
 		JpaConfig.class, // EntityManagerFactory, JpaTrasaction
-
 })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ActiveProfiles("jpa")
@@ -29,14 +41,149 @@ public class PlatRepositoryIntegrationTest {
 
 	@Test
 	public void TestFindAll() {
-
-		int taillelist = dao.findAll().size();
-		assertThat(taillelist > 0);
+		assertThat(dao.findAll()).isNotEmpty();
 	}
 
 	@Test
 	void testFindAllSortAsc() {
 
+		// List<Plat> listPlat = repo.findAll(Sort.by(Sort.Direction.ASC, "nom"));
+		List<Plat> listPlat = dao.findAll(Sort.sort(Plat.class).by(Plat::getPrixEnCentimesEuros).ascending());
+
+		assertThat(listPlat).isSortedAccordingTo(Comparator.comparing(Plat::getPrixEnCentimesEuros));
+	}
+
+	@Test
+	void testtestFindAllSortDesc() {
+
+		List<Plat> listPlat = dao.findAll(Sort.by(Sort.Direction.DESC, "nom"));
+
+		assertThat(listPlat).isSortedAccordingTo(Comparator.comparing(Plat::getNom).reversed());
+
+	}
+
+	@Test
+	void testFindAllPageable() {
+
+		Pageable firstPageWithTwoElements = PageRequest.of(0, 2, Sort.by(Sort.Direction.ASC, "nom"));
+
+		Page<Plat> pagePlat = dao.findAll(firstPageWithTwoElements);
+
+		assertThat(pagePlat.getSize()).isEqualTo(2);
+		assertThat(pagePlat.getContent().get(0).getId()).isEqualTo(4);
+
+	}
+
+	@Test
+	void testFindById() {
+		Optional<Plat> byId = dao.findById(4);
+
+		if (byId.isPresent()) {
+
+			Plat plat1 = dao.findById(4).get();
+			assertThat(plat1.getNom()).isEqualTo("Blanquette de veau");
+			assertThat(plat1.getPrixEnCentimesEuros()).isEqualTo(1500);
+
+			Plat plat = dao.findById(4)
+					.orElseThrow(() -> new RuntimeException("L'id = 4 existe, la requête devrait retourner un plat"));
+
+			assertThat(plat.getNom()).isEqualTo("Blanquette de veau");
+			assertThat(plat.getPrixEnCentimesEuros()).isEqualTo(1500);
+		}
+
+	}
+
+	@Test
+	@Transactional
+	void testGetOne() {
+
+		Plat plat = dao.getOne(2);
+
+		assertThat(plat.getNom()).isEqualTo("Moules-frites");
+		assertThat(plat.getPrixEnCentimesEuros()).isEqualTo(1300);
+	}
+
+	@Test
+	void testCount() {
+		Long total = dao.count();
+		assertThat(total).isEqualTo(6);
+	}
+
+	@Test
+	void testFindByPrix() {
+		List<Plat> platByPrix = dao.findByPrixEnCentimesEuros(1300);
+
+		assertThat(platByPrix.size()).isEqualTo(2);
+		assertThat(platByPrix.get(0).getNom()).isEqualTo("Magret de canard");
+
+	}
+
+	@Test
+	void testAvgPrix() {
+		Double moyenne = dao.findByAvgPrixSuppA(0);
+
+		Double sommeAvecFindAll = 0.0;
+
+		List<Plat> listPlat = dao.findAll();
+
+		for (Plat plat : listPlat) {
+			sommeAvecFindAll += plat.getPrixEnCentimesEuros();
+		}
+
+		double moyenneJava8 = listPlat.stream().mapToInt(Plat::getPrixEnCentimesEuros).average().orElse(0);
+
+		assertThat(moyenne).isEqualTo(sommeAvecFindAll / listPlat.size());
+		assertThat(moyenne).isEqualTo(moyenneJava8);
+
+	}
+
+	@Test
+	void findIngredientsByPlatNom() {
+
+		List<Ingredient> listIngDePlat = dao.findIngredientsByPlatNom("Moules-frites");
+
+		assertThat(listIngDePlat.size()).isEqualTo(6);
+		assertThat(listIngDePlat.get(0).getNom()).isEqualTo("Moule");
+
+	}
+
+	@Test
+	void testFindByid() {
+		Optional<Plat> p = dao.findById(1);
+		assertThat(p.get()).isEqualTo("Magret de canard");
+	}
+
+	@Test
+	@Transactional
+	void testFindByNomWithIngredients() {
+		Plat plat = dao.findIngredientByPlatNom("Magret de canard");
+		assertThat(plat.getIngredients()).extracting(Ingredient.getNom()).contains("Sel");
+
+	}
+
+	@Test
+	void testSave() {
+
+		long CountBefore = dao.count();
+		Plat plat = new Plat();
+		plat.setNom("Pizza");
+		plat.setPrixEnCentimesEuros(1400);
+
+		dao.save(plat);
+
+		long CountAfter = dao.count();
+
+		assertThat();
+	}
+
+	@Test
+	void testChangerNom() {
+
+		dao.updateNomPlat("Couscous", "Couscous-Merguez");
+
+		List<Plat> listPlat = dao.findAll();
+
+		assertThat(listPlat).extracting(Plat::getNom).contains("Couscous-Merguez", Index.atIndex(2));
 	}
 
 }
